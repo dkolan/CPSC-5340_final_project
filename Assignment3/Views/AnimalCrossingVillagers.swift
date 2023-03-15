4619d34d824350718241a6626eca4dd1339ac9e0
Dan Kolan <dan.kolan@gmail.com>
2023-03-14 19:26:27 -0500

    init models

--- Assignment3/Views/AnimalCrossingVillagers.swift
+++ Assignment3/Views/AnimalCrossingVillagers.swift
@@ -7,20 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AnimalCrossingVillagers: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AnimalCrossingVillagers()
     }
 }
