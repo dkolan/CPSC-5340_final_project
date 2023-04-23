09b25c6daf9248c320e5631e8b9d075d0ee36ff1
Dan Kolan <dan.kolan@gmail.com>
2023-04-23 15:57:09 -0500

    added accessories

---
+++ Assignment3/Models/AccessoriesModel.swift
@@ -0,0 +1,51 @@
+//
+//  AccessoriesModel.swift
+//  ACNH Reference
+//
+//  Created by Dan Kolan on 4/23/23.
+//
+
+import Foundation
+
+struct Accessories: Codable {
+    let data: [AccessoriesModel]
+}
+
+struct AccessoriesModel: Codable {
+    let url: String
+    let name: String
+    let category: String
+    let sell: Int
+    let variationTotal: Int
+    let villEquip: Bool
+    let seasonality: String
+    let versionAdded: String
+    let unlocked: Bool
+    let notes: String
+    let labelThemes: [String]
+    let styles: [String]
+    let availability: [AvailabilityModel]
+    let buy: [BuyModel]
+    let variations: [VariationModel]
+
+    enum CodingKeys: String, CodingKey {
+        case url
+        case name
+        case category
+        case sell
+        case variationTotal = "variation_total"
+        case villEquip = "vill_equip"
+        case seasonality
+        case versionAdded = "version_added"
+        case unlocked, notes
+        case labelThemes = "label_themes"
+        case styles
+        case availability
+        case buy
+        case variations
+    }
+}
+
+extension AccessoriesModel: Identifiable {
+    var id: String { return name }
+}
